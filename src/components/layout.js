926a05892396d1ac291b0c38a0738aa83a88f2fe
tetsuo shima <primaveraentalca@gmail.com>
2019-08-18 21:22:48 -0400

    add basic scene component to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Scene from "./scene"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Scene />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
---
+++ src/components/scene.js
@@ -0,0 +1,103 @@
+import React from "react"
+import * as THREE from "three"
+
+const Scene = () => {
+  const { useRef, useEffect, useState } = React
+  const mount = useRef(null)
+  const [isAnimating, setAnimating] = useState(true)
+  const controls = useRef(null)
+
+  useEffect(() => {
+    let width = mount.current.clientWidth
+    let height = mount.current.clientHeight
+    let frameId
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({
+      color: 0xff00ff,
+      wireframe: true,
+    })
+    const cube = new THREE.Mesh(geometry, material)
+
+    camera.position.z = 4
+    scene.add(cube)
+    renderer.setClearColor("#eeeeee")
+    renderer.setSize(width, height)
+
+    const renderScene = () => {
+      renderer.render(scene, camera)
+    }
+
+    const handleResize = () => {
+      width = mount.current.clientWidth
+      height = mount.current.clientHeight
+      renderer.setSize(width, height)
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+      renderScene()
+    }
+
+    const animate = () => {
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+
+      renderScene()
+      frameId = window.requestAnimationFrame(animate)
+    }
+
+    const start = () => {
+      if (!frameId) {
+        frameId = requestAnimationFrame(animate)
+      }
+    }
+
+    const stop = () => {
+      cancelAnimationFrame(frameId)
+      frameId = null
+    }
+
+    mount.current.appendChild(renderer.domElement)
+    window.addEventListener("resize", handleResize)
+    start()
+
+    controls.current = { start, stop }
+
+    return () => {
+      stop()
+      window.removeEventListener("resize", handleResize)
+      mount.current.removeChild(renderer.domElement)
+
+      scene.remove(cube)
+      geometry.dispose()
+      material.dispose()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isAnimating) {
+      controls.current.start()
+    } else {
+      controls.current.stop()
+    }
+  }, [isAnimating])
+
+  return (
+    <div
+      style={{
+        position: `fixed`,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+      ref={mount}
+      onClick={() => setAnimating(!isAnimating)}
+    />
+  )
+}
+
+export default Scene
