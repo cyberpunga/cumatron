b801b186748c22fdc5b987923bc0a02ac1d8b1fd
tetsuo shima <primaveraentalca@gmail.com>
2019-08-24 15:17:44 -0400

    use react-animation-group when dismissing confirmation dialog

--- src/components/confirm.js
+++ src/components/confirm.js
@@ -1,41 +1,47 @@
 import React, { useState } from "react"
+import { Transition } from "react-transition-group"
+import styled from "styled-components"
 
-const Confirm = props => {
-  const [val, setVal] = useState(false)
+const Confirmation = styled.div`
+  /* animation */
+  transition: 0.5s;
+  transform: translateY(
+    ${({ state }) =>
+      state === "entering" || state === "entered" ? 0 : -4000}px
+  );
+  /* defaults */
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(2, 2, 2, 0.98);
+  text-align: center;
+  font-family: "Org_v01";
+`
+const Text = styled.div`
+  color: #fefefe;
+  text-align: justify;
+  padding: 100px 50px;
+`
+
+const Button = styled.button`
+  background: rgba(0, 0, 0, 0);
+  color: #fefefe;
+`
+
+export default () => {
+  const [val, setVal] = useState(true)
   const handleClick = () => setVal(!val)
-  return !val ? (
-    <div
-      style={{
-        position: "absolute",
-        width: `100%`,
-        height: `100%`,
-        top: 0,
-        left: 0,
-        background: "rgba(2,2,2,.98)",
-        textAlign: `center`,
-        fontFamily: `Org_v01`,
-      }}
-    >
-      <div
-        style={{
-          color: `#fefefe`,
-          textAlign: `justify`,
-          padding: 100,
-        }}
-      >
-        hola *.*
-      </div>
-      <button
-        style={{
-          background: `rgba(0,0,0,0)`,
-          color: `#fefefe`,
-        }}
-        onClick={handleClick}
-      >
-        OK
-      </button>
-    </div>
-  ) : null
-}
 
-export default Confirm
+  return (
+    <Transition in={val} timeout={500} unmountOnExit>
+      {state => (
+        <Confirmation state={state}>
+          <Text>hola *.*</Text>
+          <Button onClick={handleClick}>OK!</Button>
+        </Confirmation>
+      )}
+    </Transition>
+  )
+}
