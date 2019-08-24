60f0ca450e2a65b1900cae2e6d5201fd0b3faaad
tetsuo shima <primaveraentalca@gmail.com>
2019-08-23 20:22:07 -0400

    use Query component to query poem on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const SHEETPOEM_QUERY = gql`
+  query {
+    sheetpoem(
+      spreadsheetId: "1qjgDw3TREpqQoSSbB0tzd0Joues1jraJix2mU52zToU"
+      range: "A1:A1000"
+      verses: 4
+    )
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -11,9 +22,18 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <Query
+      query={SHEETPOEM_QUERY}
+      pollInterval={30 * 1000}
+      // onCompleted={({ sheetpoem }) => set({ sheetpoem: sheetpoem })}
+    >
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>Error: {error.message}</p>
+        return <div>{data.sheetpoem}</div>
+      }}
+    </Query>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
