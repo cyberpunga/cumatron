ec9f860d7b19bf72cf63cb410ac106f562512fcb
tetsuo shima <primaveraentalca@gmail.com>
2019-09-06 14:32:50 -0400

    update apollo client

--- src/components/apollo/index.js
+++ src/components/apollo/index.js
@@ -1,7 +1,32 @@
-import ApolloClient from "apollo-boost"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
 import fetch from "isomorphic-fetch"
+import gql from "graphql-tag"
+
+const cache = new InMemoryCache()
+cache.writeData({
+  data: {
+    isLoaded: false,
+  },
+})
+
+const link = new HttpLink({
+  uri: "/.netlify/functions/graphql",
+})
+
+const typeDefs = gql`
+  extend type Query {
+    isLoaded: Boolean!
+  }
+`
+
+const resolvers = {}
 
 export const client = new ApolloClient({
-  uri: "/.netlify/functions/graphql",
+  cache,
+  link,
   fetch,
+  typeDefs,
+  resolvers,
 })
