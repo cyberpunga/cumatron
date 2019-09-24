7733876cbae48eeff1c4b672e9d36cbaeb270c4e
tetsuo shima <primaveraentalca@gmail.com>
2019-09-24 12:15:24 -0300

    use small sky image

--- src/components/scene/Sky.js
+++ src/components/scene/Sky.js
@@ -1,39 +1,37 @@
-import * as THREE from 'three';
-import { useEffect } from 'react';
-import { useThree } from '../ThreeJSManager/';
+import * as THREE from "three"
+import { useEffect } from "react"
+import { useThree } from "../ThreeJSManager/"
 
 const Sky = () => {
   const setup = context => {
-    const { scene } = context;
-    const skyGeometry = new THREE.SphereBufferGeometry(500, 32, 32);
-    skyGeometry.scale(-1, 1, 1); // invert the geometry on the x-axis so that all of the faces point inward
+    const { scene } = context
+    const skyGeometry = new THREE.SphereBufferGeometry(500, 32, 32)
+    skyGeometry.scale(-1, 1, 1) // invert the geometry on the x-axis so that all of the faces point inward
 
     const skyTexture = new THREE.TextureLoader().load(
-      'TychoSkymapII.t5_04096x02048.jpg',
-    );
-    skyTexture.minFilter = THREE.LinearFilter;
+      // 'TychoSkymapII.t5_04096x02048.jpg',
+      "small.png"
+    )
+    skyTexture.minFilter = THREE.LinearFilter
 
     const skyMaterial = new THREE.MeshBasicMaterial({
       map: skyTexture,
       side: THREE.DoubleSide,
-    });
-    const sphere = new THREE.Mesh(skyGeometry, skyMaterial);
-    scene.add(sphere);
+    })
+    const sphere = new THREE.Mesh(skyGeometry, skyMaterial)
+    scene.add(sphere)
 
-    return sphere;
-  };
+    return sphere
+  }
 
-  const { getEntity, timer } = useThree(setup);
+  const { getEntity, timer } = useThree(setup)
 
-  useEffect(
-    () => {
-      const sky = getEntity();
-      sky.rotation.y += 0.0008;
-    },
-    [timer],
-  );
+  useEffect(() => {
+    const sky = getEntity()
+    sky.rotation.y += 0.0008
+  }, [timer])
 
-  return null;
-};
+  return null
+}
 
-export default Sky;
+export default Sky
