2088fd50d73df943c83b902d205ac8e2c48b55b2
tetsuo shima <primaveraentalca@gmail.com>
2019-10-15 22:54:57 -0300

    update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `cumatron`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `cumatron recitando poemas en el vacío`,
     author: `cyberpunga`,
     author_url: `https://cyberpun.ga`,
   },
