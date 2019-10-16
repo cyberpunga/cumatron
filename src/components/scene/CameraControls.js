2d0f97c876e3708ed19d404be5a58302f8dc582b
tetsuo shima <primaveraentalca@gmail.com>
2019-10-15 22:19:02 -0300

    update camera

--- src/components/scene/CameraControls.js
+++ src/components/scene/CameraControls.js
@@ -1,37 +1,43 @@
-import * as THREE from 'three';
-import OrbitControlsDefault from 'three-orbit-controls';
-import { useEffect } from 'react';
-import { useThree } from '../ThreeJSManager/';
+import * as THREE from "three"
+import OrbitControlsDefault from "three-orbit-controls"
+import { useEffect } from "react"
+import { useThree } from "../ThreeJSManager/"
 
-const OrbitControls = OrbitControlsDefault(THREE);
+const OrbitControls = OrbitControlsDefault(THREE)
 
 const CameraControls = () => {
   const setup = ({ camera, canvas }) => {
-    const controls = new OrbitControls(camera, canvas);
-
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.12;
-    controls.rotateSpeed = 0.08;
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = 0.08;
-    controls.maxPolarAngle = Math.PI / 2;
-    controls.enableKeys = false;
+    const controls = new OrbitControls(camera, canvas)
+    // controls.enableDamping = true
+    // controls.dampingFactor = 0.12
+    // controls.rotateSpeed = 0.08
+    // controls.autoRotate = true
+    // controls.autoRotateSpeed = 0.08
+    // controls.maxPolarAngle = Math.PI / 2
+    controls.enableKeys = false
     // controls.update();
 
-    return controls;
-  };
+    controls.enableDamping = true // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.01 // friction
+    controls.rotateSpeed = 0.01 // mouse sensitivity
+    // controls.autoRotate = true
+    controls.autoRotateSpeed = 0.02
+    controls.enableZoom = true
+    controls.screenSpacePanning = false
+    controls.minDistance = 10
+    controls.maxDistance = 480
+    controls.maxPolarAngle = Math.PI
+    return controls
+  }
 
-  const { getEntity, timer } = useThree(setup);
+  const { getEntity } = useThree(setup)
 
-  useEffect(
-    () => {
-      const controls = getEntity();
-      controls.update(); // required for damping effect
-    },
-    [timer],
-  );
+  useEffect(() => {
+    const controls = getEntity()
+    controls.update() // required for damping effect
+  })
 
-  return null;
-};
+  return null
+}
 
-export default CameraControls;
+export default CameraControls
