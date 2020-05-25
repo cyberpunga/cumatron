5ee82c3982a2d84e389bad33cf3b5709cbd32813
tetsuo shima <primaveraentalca@gmail.com>
2020-05-25 13:31:06 -0400

    remove HTML component from drei

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-import { HTML, Sky } from "drei"
+import { Sky } from "drei"
 
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 const NotFoundPage = () => {
-  const isSSR = typeof window === "undefined"
-
   return (
     <Layout>
       <SEO title="404 :(" />
-      {!isSSR && (
-        <HTML center style={{ width: 400 }}>
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that does not exist... the sadness.</p>
-          <Link to="/">lala</Link>
-        </HTML>
-      )}
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that does not exist... the sadness.</p>
+      <Link to="/">lala</Link>
       <Sky />
     </Layout>
   )
