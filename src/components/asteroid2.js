e04bcdbd38b9914df3b4ad5b0f5086814d0c6053
tetsuo shima <primaveraentalca@gmail.com>
2020-07-06 20:58:49 -0400

    useLoader with multiple textures example

--- src/components/asteroid2.js
+++ src/components/asteroid2.js
@@ -1,39 +1,21 @@
-import React, { useRef, useMemo } from "react"
-import { useFrame } from "react-three-fiber"
+import React from "react"
+import { useLoader } from "react-three-fiber"
 import { TextureLoader } from "three"
 
 const Asteroid = props => {
-  const ref = useRef()
-  const material = useRef()
-  const texture = useMemo(() => new TextureLoader().load("/asteroides.png"), [])
-  const texture2 = useMemo(
-    () => new TextureLoader().load("/asteroides2.png"),
-    []
-  )
-  const texture3 = useMemo(
-    () => new TextureLoader().load("/asteroides3.png"),
-    []
-  )
-  const texture4 = useMemo(
-    () => new TextureLoader().load("/asteroides4.png"),
-    []
-  )
-  // texture.minFilter = LinearFilter
-  const textures = [texture, texture2, texture3, texture4]
-
-  useFrame(({ clock: { elapsedTime } }) => {
-    // ref.current.position.x = 50 * props.i * Math.cos(elapsedTime * 0.001 * props.i)
-    // ref.current.position.z = 50 * props.i * Math.sin(elapsedTime * 0.001 * props.i)
-    material.current.rotation += 0.001 * props.i
-  })
+  const [texture, texture2] = useLoader(TextureLoader, [
+    "/asteroides4.png",
+    "/asteroides2.png",
+  ])
   return (
-    <sprite ref={ref} {...props}>
-      <spriteMaterial
-        ref={material}
-        attach="material"
-        map={textures[Math.floor(Math.random() * textures.length)]}
-      />
-    </sprite>
+    <group {...props}>
+      <sprite position={[0, 0, -800]} scale={[400, 300]}>
+        <spriteMaterial attach="material" map={texture} rotation={Math.PI} />
+      </sprite>
+      <sprite position={[-100, 200, 800]} scale={[400, 400]}>
+        <spriteMaterial attach="material" map={texture2} />
+      </sprite>
+    </group>
   )
 }
 
