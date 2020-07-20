35eb4dbf40edefcb53fb30a7bec6385135d1ae42
tetsuo shima <primaveraentalca@gmail.com>
2020-07-20 11:47:27 -0400

    use phong material

--- src/components/001/text.js
+++ src/components/001/text.js
@@ -1,21 +1,23 @@
 import React from "react"
-// import { useThree } from "react-three-fiber"
 import { Text } from "drei"
+import { MeshPhongMaterial, DoubleSide } from "three"
 
 import font from "orgdot-org-v01/Orgv01.woff"
 
 export default props => {
-  // const { viewport } = useThree()
   return (
     <Text
-      // maxWidth={(viewport.width / 100) * 90}
-      position={[0, 2, -20]}
-      rotation={[0.1, 0, 0.1]}
+      material={
+        new MeshPhongMaterial({
+          side: DoubleSide,
+          transparent: true,
+          opacity: 0.54,
+        })
+      }
+      position={[0, 2, -10]}
+      rotation={[-0.54, 0, 0.1]}
       fontSize={2}
-      color={0xeeeeee}
       font={font}
-      anchorX="center"
-      anchorY="middle"
       {...props}
     >
       {props.children}
