4c06384a5d65c7e5ec72830e9295d950865bc071
Rodrigo Moreira <hola@piducan.dev>
2022-02-03 16:50:06 -0300

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Canvas } from "react-three-fiber"
-import { HTML, Sky } from "drei"
+import React from "react";
+import { Link } from "gatsby";
 
-import SEO from "../components/seo"
-import Layout from "../components/layout"
+import SEO from "../components/seo";
+import Layout from "../components/layout";
 
 const NotFoundPage = () => {
-  const isSSR = typeof window === "undefined"
   return (
     <Layout>
       <SEO title="404 :(" />
-      <Canvas pixelRatio={isSSR ? null : window.devicePixelRatio}>
-        {!isSSR && (
-          <HTML center style={{ width: 400 }}>
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that does not exist... the sadness.</p>
-            <Link to="/">lala</Link>
-          </HTML>
-        )}
-        <Sky />
-      </Canvas>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that does not exist... the sadness.</p>
+      <Link to="/">lala</Link>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
