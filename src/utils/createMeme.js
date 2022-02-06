35dc6561dd631942fce8374075f388ec5918258d
Rodrigo Moreira <hola@piducan.dev>
2022-02-06 05:55:27 -0300

    use @piducancore/gleech

--- src/utils/createMeme.js
+++ src/utils/createMeme.js
@@ -1,26 +1,28 @@
 const path = require("path");
 const Jimp = require("jimp");
-const gleech = require("gleech");
+const gleech = require("@piducancore/gl33ch/src");
+
+async function glitch(input) {
+  const image = await gleech.read(input);
+  await image.drumrollVerticalWave();
+  await image.fractal(Math.floor(Math.random()));
+  await image.colorShift();
+  return image;
+}
+
+async function pasteText(image, text) {
+  const font = await Jimp.loadFont(path.join(__dirname, "../../static/org_01.fnt"));
+  const alignmentX = Jimp.HORIZONTAL_ALIGN_LEFT;
+  const alignmentY = Jimp.VERTICAL_ALIGN_BOTTOM;
+  image.print(font, 32, 0, { text, alignmentX, alignmentY }, image.bitmap.width - 32, image.bitmap.height);
+  const imageBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+  return imageBuffer;
+}
 
 async function createMeme(image, text) {
-  const font = await Jimp.loadFont(path.join(__dirname, "../../static/org_01.fnt"));
-  const xoxo = await gleech.read(image);
-  xoxo.quality(60);
-
-  await xoxo.drumrollVerticalWave();
-  await xoxo.fractal(Math.floor(Math.random()));
-  await xoxo.colorShift();
-  await xoxo.print(
-    font,
-    32,
-    0,
-    { text, alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT, alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM },
-    xoxo.bitmap.width - 32,
-    xoxo.bitmap.height
-  );
-
-  const buffer = await xoxo.getBufferAsync(Jimp.MIME_JPEG);
-  return buffer;
+  const glitchedImage = await glitch(image);
+  const meme = await pasteText(glitchedImage, text);
+  return meme;
 }
 
 module.exports = { createMeme };
