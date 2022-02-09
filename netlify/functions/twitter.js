e1a385a3079afd1e9035017c724a2602c8e10238
Rodrigo Moreira <hola@piducan.dev>
2022-02-09 10:52:26 -0300

    add try catch blocks in scheduled function

--- netlify/functions/twitter.js
+++ netlify/functions/twitter.js
@@ -55,16 +55,20 @@ async function handleTweetCreateEvents(event) {
 }
 
 async function handleScheduleEvents() {
-  const sheetpoem = await sheetpoetry("poem");
-  const images = await getResources();
-  const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
-  const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
-  const meme = await createMeme(random, sheetpoem);
-  const { media_id_string } = await uploadImage({ media_data: meme.toString("base64") });
-  await tweet({
-    status: sheetpoem,
-    media_ids: media_id_string,
-  });
+  try {
+    const sheetpoem = await sheetpoetry("poem");
+    const images = await getResources();
+    const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+    const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
+    const meme = await createMeme(random, sheetpoem);
+    const { media_id_string } = await uploadImage({ media_data: meme.toString("base64") });
+    await tweet({
+      status: sheetpoem,
+      media_ids: media_id_string,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 exports.handler = async (req) => {
