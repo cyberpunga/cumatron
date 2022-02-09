d4dde1208cc4e976852b64d892c2149c0e34f47c
Rodrigo Moreira <hola@piducan.dev>
2022-02-09 07:37:12 -0300

    scheduled tweets

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,27 @@
+const { sheetpoetry } = require("../../src/utils");
+const { getResources } = require("./cloudinary");
+const { createMeme } = require("../../src/utils/createMeme");
+const { tweet, uploadImage } = require("../../src/utils/twitter");
+
+const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+
 exports.handler = async (req) => {
   try {
-    console.log(req);
+    const sheetpoem = await sheetpoetry("poem");
+    const images = await getResources();
+    const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
+    const meme = await createMeme(random, sheetpoem);
+
+    const { media_id_string } = await uploadImage({
+      media_data: meme,
+    });
+    await tweet({
+      status: sheetpoem,
+      media_ids: media_id_string,
+    });
     return {
       statusCode: 200,
-      body: "hello",
+      body: "OK",
     };
   } catch (error) {
     throw error;
