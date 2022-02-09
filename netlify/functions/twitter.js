65de94187466236b5c71cb1123e0962eb8d825de
Rodrigo Moreira <hola@piducan.dev>
2022-02-09 11:02:21 -0300

    debug

--- netlify/functions/twitter.js
+++ netlify/functions/twitter.js
@@ -57,11 +57,14 @@ async function handleTweetCreateEvents(event) {
 async function handleScheduleEvents() {
   try {
     const sheetpoem = await sheetpoetry("poem");
+    console.log(sheetpoem);
     const images = await getResources();
     const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
     const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
+    console.log(random);
     const meme = await createMeme(random, sheetpoem);
     const { media_id_string } = await uploadImage({ media_data: meme.toString("base64") });
+    console.log(media_id_string);
     await tweet({
       status: sheetpoem,
       media_ids: media_id_string,
