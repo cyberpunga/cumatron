3030ec3d2573eba8626a09b7e4041bbe059a9fbd
Rodrigo Moreira <hola@piducan.dev>
2022-02-09 09:36:05 -0300

    debug scheduled function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,24 +1,25 @@
-const { sheetpoetry } = require("../../src/utils");
-const { getResources } = require("../../src/utils/cloudinary");
-const { createMeme } = require("../../src/utils/createMeme");
-const { tweet, uploadImage } = require("../../src/utils/twitter");
+// const { sheetpoetry } = require("../../src/utils");
+// const { getResources } = require("../../src/utils/cloudinary");
+// const { createMeme } = require("../../src/utils/createMeme");
+// const { tweet, uploadImage } = require("../../src/utils/twitter");
 
-const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+// const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
 
 exports.handler = async (req) => {
+  console.log(req);
   try {
-    const sheetpoem = await sheetpoetry("poem");
-    const images = await getResources();
-    const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
-    const meme = await createMeme(random, sheetpoem);
+    // const sheetpoem = await sheetpoetry("poem");
+    // const images = await getResources();
+    // const random = pickRandomElement(images).secure_url.replace(/\/upload\/v([0-9])\w+\//g, "/upload/w_480/");
+    // const meme = await createMeme(random, sheetpoem);
 
-    const { media_id_string } = await uploadImage({
-      media_data: meme.toString("base64"),
-    });
-    await tweet({
-      status: sheetpoem,
-      media_ids: media_id_string,
-    });
+    // const { media_id_string } = await uploadImage({
+    //   media_data: meme.toString("base64"),
+    // });
+    // await tweet({
+    //   status: sheetpoem,
+    //   media_ids: media_id_string,
+    // });
     return {
       statusCode: 200,
       body: "OK",
